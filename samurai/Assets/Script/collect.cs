7259ab832ecc398911a24ceee4a20c21ffcc544d
Sorokina <msdashaback@gmail.com>
2023-05-25 19:33:39 +0200

    Initial commit

---
+++ samurai/Assets/Script/collect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+public class collect : MonoBehaviour
+{
+
+    private bool hasCollided = false;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!hasCollided)
+        {
+            hasCollided = true;
+            Destroy(gameObject);
+            score.scoreAmount += 1;
+        }
+    }
+}
---
+++ samurai/Assets/Script/trigger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class trigger : MonoBehaviour
+{
+	[System.Obsolete]
+	void OnTriggerEnter2D(Collider2D col)
+	{
+			Application.LoadLevel(Application.loadedLevel);
+	}
+}
